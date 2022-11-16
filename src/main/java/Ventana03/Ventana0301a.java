d5f4559c8112bb44854b55f2089f96751cf31dfd
LAB-2 <LAB-2@DESKTOP-9OQBEH7>
2022-11-16 12:46:34 -0500

    Terminado

---
+++ src/main/java/Ventana03/Ventana0301a.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ventana03;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.List;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author LAB-2
+ */
+public class Ventana0301a extends JFrame {
+    private final List<JPanel> jPanelList;
+
+    public Ventana0301a(String title) throws HeadlessException {
+        super(title);
+        this.setSize(800, 600);
+        this.setLocation(100, 200);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.jPanelList= new ArrayList<>();
+        this.setLayout(new FlowLayout());
+        
+        
+    }
+    public void iniciarComponentes() {
+        var jPanelrojo = new JPanel();
+        jPanelrojo.setBackground(Color.RED);
+        var jPanelazul = new JPanel();
+        jPanelazul.setBackground(Color.BLUE);
+        var jPanelverde = new JPanel();
+        jPanelverde.setBackground(Color.GREEN);
+        var jPaneldark = new JPanel();
+        jPaneldark.setBackground(Color.DARK_GRAY);
+        var jPanelcyan = new JPanel();
+        jPanelcyan.setBackground(Color.cyan);
+        
+        this.jPanelList.add(jPanelrojo);
+        this.jPanelList.add(jPanelazul);
+        this.jPanelList.add(jPanelverde);
+        this.jPanelList.add(jPaneldark);
+        this.jPanelList.add(jPanelcyan);
+        for(var jPanel:this.jPanelList)
+        this.getContentPane().add(jPanel);
+    }
+}
+    
+  
+    
+    
+    
+
--- src/main/java/com/mycompany/ventanas/principal/VentanasPrincipal.java
+++ src/main/java/com/mycompany/ventanas/principal/VentanasPrincipal.java
@@ -4,7 +4,7 @@
  */
 package com.mycompany.ventanas.principal;
 
-import javax.swing.JFrame;
+import Ventana03.Ventana0301a;
 
 /**
  *
@@ -13,33 +13,9 @@ import javax.swing.JFrame;
 public class VentanasPrincipal {
 
     public static void main(String[] args) {
-        var ventana0301a = new JFrame();
-        ventana0301a.setTitle("Ventana Unidad 03.01a");
-        ventana0301a.setSize(800, 600);
-        ventana0301a.setLocation(200, 100);
-        ventana0301a.setVisible(true);
-        ventana0301a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        var ventana0302a = new JFrame();
-        ventana0302a.setTitle("Ventana Unidad 03.02a");
-        ventana0302a.setSize(1284, 800);
-        ventana0302a.setLocation(300, 200);
-        ventana0302a.setVisible(true);
-        ventana0302a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        var ventana0303a = new JFrame();
-        ventana0303a.setTitle("Ventana Unidad 03.03a");
-        ventana0303a.setSize(1366, 768);
-        ventana0303a.setLocation(400, 100);
-        ventana0303a.setVisible(true);
-        ventana0303a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        var ventana0304a = new JFrame();
-        ventana0304a.setTitle("Ventana Unidad 03.04a");
-        ventana0304a.setSize(1200, 500);
-        ventana0304a.setLocation(800, 400);
-        ventana0304a.setVisible(true);
-        ventana0304a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-}
+        var ventana1=new Ventana0301a("Ecuador");
+        ventana1.iniciarComponentes();
+        ventana1.setVisible(true);
+    }}
